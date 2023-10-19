b52b335e39e6b517c3cbca62297a32597db05bf5
Rukky <abrukevwe@gmail.com>
2023-10-19 04:20:18 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from './components/NavBar';
+import MainContent from "./components/MainContent";
 import './styles/index.css';
 
 function App() {
     return (
         <div className="Container">
             <NavBar />
+            <MainContent />
         </div>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,17 +2,17 @@ import React from "react";
 import logo from "../images/logo.svg"
 import "../styles/navbar.css"
 
-function NavBar () {
+function NavBar() {
     return (
         <nav>
             <div className="Nav-logo">
-                <img src = {logo} alt="Brand Logo" />
+                <img src={logo} alt="Brand Logo" />
                 <h3>ReactFacts</h3>
             </div>
             <div className="Nav-right">
                 <h4>React Course - Project 1</h4>
             </div>
-           
+
         </nav>
     )
 }
